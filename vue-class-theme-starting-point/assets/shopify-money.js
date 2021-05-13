5369ab389c7b56e572f0ebfc3119c6517eb1f55d
SzubaKrzysztof <szuba.christopher@gmail.com>
2021-05-13 23:11:41 +0200

    third commit after cart is interactive

--- vue-class-theme-starting-point/assets/shopify-money.js
+++ vue-class-theme-starting-point/assets/shopify-money.js
@@ -25,7 +25,7 @@ Shopify.formatMoney = function(cents, format) {
     number = (number/100.0).toFixed(precision);
 
     var parts   = number.split('.'),
-        dollars = parts[0].replace(/(\d)(?=(\d\d\d)+(?!\d))/g, '$1' + thousands),
+        dollars = parts[0].replace(/(\d)(?=(\d\d\d)+(?!\d))/g, '??1' + thousands),
         cents   = parts[1] ? (decimal + parts[1]) : '';
 
     return dollars + cents;
