c976bdeb6455d9df667083b632a6f6487cf8f17d
SzubaKrzysztof <szuba.christopher@gmail.com>
2021-05-17 14:53:29 +0200

    added project covid-tracker

---
+++ vue-covid-tracker/covid-tracker/src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+  <header class="text-center bg-blue-800 text-white p-4 mb-5">
+    <div class="text-3xl md:text-5xl font-bold mb-3"><a class="text-blue-300" href="https://covid19api.com" target="_blank"><i class="fa fa-viruses"></i></a>Covid Tracker</div>
+    <p>API by <a class="text-blue-300" href="https://covid19api.com" target="_blank">covid19api.com</a></p>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
