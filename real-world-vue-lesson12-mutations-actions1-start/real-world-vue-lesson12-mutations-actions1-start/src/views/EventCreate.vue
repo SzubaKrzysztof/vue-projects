f3991d3c6dd26d26dd598d9738ff7404def4ea14
SzubaKrzysztof <szuba.christopher@gmail.com>
2021-06-10 13:38:53 +0200

    next project added

---
+++ real-world-vue-lesson12-mutations-actions1-start/real-world-vue-lesson12-mutations-actions1-start/src/views/EventCreate.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>Create an Event, {{ user.name }}</h1>
+    <p>This event was created by {{ user.name }}</p>
+    <p>{{ getEventById(2) }}</p>
+  </div>
+</template>
+
+<script>
+import { mapState, mapGetters } from 'vuex'
+
+export default {
+  computed: {
+    ...mapGetters(['getEventById']),
+    ...mapState(['user', 'categories'])
+    // zamiast ...mapState może być
+    // userNmae(){
+    // return this.$store.state.user.name
+    // }
+  }
+}
+</script>
+
+<style scoped>
+</style>
